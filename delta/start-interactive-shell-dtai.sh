cd223a773a885e0afd7dd65426949011cb2ce712
Shengjie Ma <sm138@illinois.edu>
2025-05-11 23:14:58 -0500

    new slurm scripts

--- delta/start-interactive-shell-dtai.sh
+++ delta/start-interactive-shell-dtai.sh
@@ -1,17 +1,17 @@
 # Use this command to start an interactive job on Delta AI.
 # Used for training
 
-srun --account=becw-dtai-gh --partition=ghx4 --time=300 \
+srun --account=becw-dtai-gh --partition=ghx4 --time=1200 \
   --nodes=1 --gpus-per-node=1 --ntasks-per-node=1 \
   --cpus-per-task=72 --mem=100g \
   --pty bash
 
-srun --account=becw-dtai-gh --partition=ghx4 --time=300 \
+srun --account=becw-dtai-gh --partition=ghx4 --time=1200 \
   --nodes=1 --gpus-per-node=2 --ntasks-per-node=1 \
   --cpus-per-task=144 --mem=200g \
   --pty bash
 
-srun --account=becw-dtai-gh --partition=ghx4 --time=960 \
+srun --account=becw-dtai-gh --partition=ghx4 --time=1200 \
   --nodes=1 --gpus-per-node=4 --ntasks-per-node=1 \
   --cpus-per-task=288 --mem=400g \
   --pty bash
--- delta/start-interactive-shell.sh
+++ delta/start-interactive-shell.sh
@@ -1,8 +1,7 @@
 # Use this command to start an interactive job on the server
 # This starts an interactive shell with 1 A40 GPU, good for experimenting with code
-# The job will shutdown after 60 minutes
 
-srun --account=becw-delta-gpu --partition=gpuA40x4-interactive --time=60 \
+srun --account=becw-delta-gpu --partition=gpuA40x4 --time=1200 \
   --nodes=1 --gpus-per-node=1 --ntasks-per-node=1 \
-  --cpus-per-task=16 --mem=50g --constraint="projects" \
+  --cpus-per-task=16 --mem=50g \
   --pty bash
