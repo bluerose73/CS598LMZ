62b62f7a70d8edcb6c3a7718571200a8f07b9d8b
Shengjie Ma <sm138@illinois.edu>
2025-05-11 23:03:26 -0500

    remove deprecated generation

--- generation/types.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class CompletionObject(BaseModel):
-    completion: str
-    prompt: str
-    num_prompt_tokens: int
-    num_completion_tokens: int
-    latency: float | None = None
